b2ab8b16cc79d01e827dd89b4943511bd6fd6264
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-04-26 01:13:12 +0500

    word counter npm publish
    npx arfat-asif-word-counter

---
+++ main.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.yellowBright("\n\t\tArfat Asif - word counter"));
+console.log("=".repeat(50));
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence",
+    }
+]);
+let word = answers.sentence.trim().split(" ");
+console.log("=".repeat(50));
+console.log(chalk.bold("-sentence words:"));
+console.log(word);
+console.log(chalk.bold(`\n - word count: ${chalk.bold.blue(word.length)}`));
+console.log("=".repeat(50));
